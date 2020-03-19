b4281848f541da078056c9da149ff9203dd31035
kborys1 <kborys@gmail.com>
2020-03-18 21:57:17 -0400

    quiz questions

--- main.R
+++ main.R
@@ -65,3 +65,48 @@ summary(cr)
 ## -1.00000 -0.05282  0.10718  0.13684  0.27831  1.00000
 length(cr)
 ## [1] 323
+
+
+##------------------------------------------------------------------------------
+## quiz
+##------------------------------------------------------------------------------
+print(getwd())
+source("pollutantmean.R")
+source("complete.R")
+source("corr.R")
+
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "sulfate", 34)
+pollutantmean("specdata", "nitrate")
+cc <- complete("specdata", c(6, 10, 20, 34, 100, 200, 310))
+print(cc$n)
+cc <- complete("specdata", 54)
+print(cc$n)
+
+RNGversion("3.5.1")  
+set.seed(42)
+cc <- complete("specdata", 332:1)
+use <- sample(332, 10)
+print(cc[use, "n"]) ## dont know
+
+cr <- corr("specdata")                
+cr <- sort(cr)   
+RNGversion("3.5.1")
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
+
+cr <- corr("specdata", 129)                
+cr <- sort(cr)                
+n <- length(cr)    
+RNGversion("3.5.1")
+set.seed(197)                
+out <- c(n, round(cr[sample(n, 5)], 4))
+print(out)
+
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))
